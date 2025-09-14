13682e55aed68462767e1e8b4948d4717187a2d8
Md Mozahidul Islam <mozahidulislamoshe01@gmail.com>
2025-09-14 23:25:21 +0600

    remove it

--- Easy/21. Merge Two Sorted Lists.py
+++ Easy/21. Merge Two Sorted Lists.py
@@ -1,7 +1,7 @@
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-       
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# class Solution:
+#     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+     
